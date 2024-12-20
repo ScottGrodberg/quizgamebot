a01dda9386a65672255428e76fa4837506dbff8c
Scott Grodberg <scott@trwebco.com>
2024-12-20 17:23:40 -0500

    Create the ping command. Coded command and message handler functions.

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-const {Client} = require("discord.js");
-const {token} = require("./config.json");
+const { Client, Events, SlashCommandBuilder, GatewayIntentBits } = require("discord.js");
+const { token } = require("./config.json");
 
-const client= new Client({intents:[]});
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+
+client.once(Events.ClientReady, client => {
+    console.log(`Logged in as ${client.user.tag}`);
+
+    const slashCommandPing = new SlashCommandBuilder().setName("ping").setDescription("Replies with pong").toJSON();
+    client.application.commands.create(slashCommandPing, "1008709138110488586").then(acs => {
+        console.log(acs);
+    });
+});
+
+client.on(Events.InteractionCreate, interaction => {
+    console.log(interaction);
+});
+
+client.on(Events.MessageCreate, message => {
+    console.log(message);
+});
 
 client.login(token);
