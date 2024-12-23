654345416aaf58e2f4576d7086cc7f970e36811b
Scott Grodberg <scott@trwebco.com>
2024-12-23 02:03:17 -0500

    Added the commandName variable to the interface.

--- commands/ICommand.ts
+++ commands/ICommand.ts
@@ -1,6 +1,7 @@
 import { Interaction, SlashCommandBuilder } from "discord.js";
 
 export interface ICommand {
+    commandName:string;
     getCommand() : SlashCommandBuilder;
     processCommand(interaction:Interaction) : void;
 }
